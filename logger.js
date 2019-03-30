319feeae32ebd7fa79f74e4533f3c3b2e9f35496
FerAiwa <fer.aiwa@gmail.com>
2019-03-30 22:40:37 +0100

    Added spaceship factory and more

---
+++ logger.js
@@ -0,0 +1,11 @@
+class Ship {
+    constructor(name, hp, shield, state, shield) {
+        this.name = name;
+        this.hp = hp;
+        this.shield = shield;
+        this.state = state;
+    }
+
+
+    
+}
